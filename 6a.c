07cd00acfc2cca11b46ac025a2412878d8447a05
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-01-23 16:36:41 +0530

    Create 6a.c

---
+++ 6a.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int yr;
+printf("enter the yr");
+scanf("%d",&yr);
+if(yr%4==0
+{
+if(yr%100==0)
+{
+if(yr%400==0)
+{
+printf("%d is leap yr", yr);
+}
+else
+printf("%d is not leap yr", yr);
+}
+else
+printf("%d is leap yr", yr);
+}
+printf("%d is not leap yr", yr);
+return o;
+}
