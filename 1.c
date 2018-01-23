ccae1816cc2a6ba1562db971dfa114c7d0444f13
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-01-23 16:08:42 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num>0)
+{
+printf("the no is positive");
+}
+else
+{
+printf("the no is negative");
+}
+}
