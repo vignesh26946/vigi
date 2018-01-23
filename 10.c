0e86ddfa746bec94a6e0dbe92cc13ca307d6e6f7
vignesh26946 <31505070+vignesh26946@users.noreply.github.com>
2018-01-23 16:45:50 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int s;
+int count=0;
+printf("enter the number");
+scanf("%d",&s);
+while(i!=0)
+{
+s/=0;
+count++;
+}
+printf("the number of digits: %d",count);
+}
